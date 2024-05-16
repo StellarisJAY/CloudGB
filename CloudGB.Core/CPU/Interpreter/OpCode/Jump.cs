90c9144e074d541d6a1580d3aaaab1fab047fa6e
xxjay <760409558@qq.com>
2024-05-16 16:56:05 +0800

    feat: JR cc,n

--- CloudGB.Core/CPU/Interpreter/OpCode/Jump.cs
+++ CloudGB.Core/CPU/Interpreter/OpCode/Jump.cs
@@ -9,5 +9,27 @@ namespace CloudGB.Core.CPU.Interpreter.OpCode
             memory.Read16Bit((ushort)(context.PC + 1), out ushort addr);
             context.PC = addr;
         }
+
+        public static void JRCondition(CPUContext context, Instruction instruction, IMemoryMap memory)
+        {
+            memory.Read((ushort)(context.PC + 1), out byte offset);
+            sbyte signedOff = (sbyte)offset;
+            switch (instruction.Opcode)
+            {
+                case 0x20:
+                    signedOff = (sbyte)(!context.ZeroFlag ? signedOff : 2);
+                    break;
+                case 0x28:
+                    signedOff = (sbyte)(context.ZeroFlag ? signedOff : 2);
+                    break;
+                case 0x30:
+                    signedOff = (sbyte)(!context.CarryFlag ? signedOff : 2);
+                    break;
+                case 0x38:
+                    signedOff = (sbyte)(context.CarryFlag ? signedOff : 2);
+                    break;
+            }
+            context.PC = (ushort)(context.PC + signedOff);
+        }
     }
 }
