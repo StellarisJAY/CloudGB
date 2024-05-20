ecc295dd75972a8efd8061002d44d168304726eb
xxjay <760409558@qq.com>
2024-05-20 16:26:49 +0800

    feat: empty memory constructor

--- CloudGB.Core/Memory/DefaultMemoryMap.cs
+++ CloudGB.Core/Memory/DefaultMemoryMap.cs
@@ -38,43 +38,52 @@ namespace CloudGB.Core.Memory
             HighRAM = new byte[128];
         }
 
+        public DefaultMemoryMap()
+        {
+            ROM0 = new byte[0x4000];
+            ROM1 = new byte[0x4000];
+            WRAM0 = new byte[4096];
+            WRAM1 = new byte[4096];
+            HighRAM = new byte[128];
+        }
+
         public bool Read(ushort address, out byte data)
         {
             bool readable = true;
             data = 0;
             switch (address)
             {
-            case <= 0x3FFF:
-                data = ROM0[address];
-                break;
-            case <= 0x7FFF:
-                data = ROM1[address - 0x4000];
-                break;
-            case <= 0x9FFF:
-                break;
-            case <= 0xBFFF:
-                break;
-            case <= 0xCFFF:
-                data = WRAM0[address & 0xFFF];
-                break;
-            case <= 0xDFFF:
-                data = WRAM1[address & 0xFFF];
-                break;
-            case <= 0xFDFF:
-                readable = false;
-                break;
-            case <= 0xFE9F:
-                break;
-            case <= 0xFEFF:
-                readable = false;
-                break;
-            case <= 0xFF7F:
-                break;
-            case <= 0xFFFE:
-                break;
-            case 0xFFFF:
-                data = InterruptEnable;
-                break;
+                case <= 0x3FFF:
+                    data = ROM0[address];
+                    break;
+                case <= 0x7FFF:
+                    data = ROM1[address - 0x4000];
+                    break;
+                case <= 0x9FFF:
+                    break;
+                case <= 0xBFFF:
+                    break;
+                case <= 0xCFFF:
+                    data = WRAM0[address & 0xFFF];
+                    break;
+                case <= 0xDFFF:
+                    data = WRAM1[address & 0xFFF];
+                    break;
+                case <= 0xFDFF:
+                    readable = false;
+                    break;
+                case <= 0xFE9F:
+                    break;
+                case <= 0xFEFF:
+                    readable = false;
+                    break;
+                case <= 0xFF7F:
+                    break;
+                case <= 0xFFFE:
+                    break;
+                case 0xFFFF:
+                    data = InterruptEnable;
+                    break;
             }
             return readable;
         }
@@ -84,34 +93,36 @@ namespace CloudGB.Core.Memory
             bool writable = true;
             switch (address)
             {
-            case <= 0x3FFF:
-                break;
-            case <= 0x7FFF:
-                break;
-            case <= 0x9FFF:
-                break;
-            case <= 0xBFFF:
-                break;
-            case <= 0xCFFF:
-                WRAM0[address & 0xFFF] = data;
-                break;
-            case <= 0xDFFF:
-                WRAM1[address & 0xFFF] = data;
-                break;
-            case <= 0xFDFF:
-                writable = false;
-                break;
-            case <= 0xFE9F:
-                break;
-            case <= 0xFEFF:
-                writable = false;
-                break;
-            case <= 0xFF7F:
-                break;
-            case <= 0xFFFE:
-                break;
-            case 0xFFFF:
-                InterruptEnable = data;
+                case <= 0x3FFF:
+                    ROM1[address] = data;
+                    break;
+                case <= 0x7FFF:
+                    ROM1[address - 0x4000] = data;
+                    break;
+                case <= 0x9FFF:
+                    break;
+                case <= 0xBFFF:
+                    break;
+                case <= 0xCFFF:
+                    WRAM0[address & 0xFFF] = data;
+                    break;
+                case <= 0xDFFF:
+                    WRAM1[address & 0xFFF] = data;
+                    break;
+                case <= 0xFDFF:
+                    writable = false;
+                    break;
+                case <= 0xFE9F:
+                    break;
+                case <= 0xFEFF:
+                    writable = false;
+                    break;
+                case <= 0xFF7F:
+                    break;
+                case <= 0xFFFE:
+                    break;
+                case 0xFFFF:
+                    InterruptEnable = data;
                 break;
             }
             return writable;
