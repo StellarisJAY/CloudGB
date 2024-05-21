9deedd15b514192b5b48f73d2ba004978b706cef
xxjay <760409558@qq.com>
2024-05-21 16:24:58 +0800

    feat: RST restart

--- CloudGB.Core/CPU/Interpreter/OpCode/System.cs
+++ CloudGB.Core/CPU/Interpreter/OpCode/System.cs
@@ -15,5 +15,39 @@ namespace CloudGB.Core.CPU.Interpreter.OpCode
             ctx.PC += 1;
             ctx.Halt = true;
         }
+
+        public static void RST(CPUContext context, Instruction ins, IMemoryMap memory)
+        {
+            PushStack16(context, memory, (ushort)(context.PC + 1));
+            ushort target = 0x0000;
+            switch(ins.Opcode)
+            {
+                case 0xC7:
+                    target += 0x00;
+                    break;
+                case 0xCF:
+                    target += 0x08;
+                    break;
+                case 0xD7:
+                    target += 0x10;
+                    break;
+                case 0xDF:
+                    target += 0x18;
+                    break;
+                case 0xE7:
+                    target += 0x20;
+                    break;
+                case 0xEF:
+                    target += 0x28;
+                    break;
+                case 0xF7:
+                    target += 0x30;
+                    break;
+                case 0xFF:
+                    target += 0x38;
+                    break;
+            }
+            context.PC = target;
+        }
     }
 }
