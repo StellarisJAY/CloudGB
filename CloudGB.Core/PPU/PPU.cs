692597ffebedeb156f4435d4fcde2d79d8db1f1a
xxjay <760409558@qq.com>
2024-05-24 15:26:31 +0800

    feat: ppu dots and scanlines

--- CloudGB.Core/PPU/PPU.cs
+++ CloudGB.Core/PPU/PPU.cs
@@ -1,4 +1,6 @@
-﻿namespace CloudGB.Core.PPU
+﻿using CloudGB.Core.Memory;
+
+namespace CloudGB.Core.PPU
 {
     public class GraphicProcessor
     {
@@ -12,10 +14,63 @@
         private LCDControll _lcdControll;
         private LCDStatus _lcdStatus;
 
-        public GraphicProcessor()
+        private byte[] _frame;
+
+        private IMemoryMap _memory;
+
+        private int _dots = 0;
+        private int _scanline = 0;
+        
+        public GraphicProcessor(IMemoryMap memory)
         {
             _lcdControll = new LCDControll();
             _lcdStatus = new LCDStatus();
+            _frame = new byte[FrameWidth * FrameHeight * 3];
+            _memory = memory;
+        }
+
+        public void Step(int cpuCycles)
+        {
+            if (_scanline >= 144)
+            {
+                _lcdStatus.Mode = LCDStatus.PPUMode.VBlank;
+            }else
+            {
+                _dots += cpuCycles;
+                if (_dots <= 80)
+                {
+                    _lcdStatus.Mode = LCDStatus.PPUMode.OAMScan;
+                }else if (_dots <= 80 + 172)
+                {
+                    _lcdStatus.Mode = LCDStatus.PPUMode.Drawing;
+                }else if (_dots < 456)
+                {
+                    _lcdStatus.Mode = LCDStatus.PPUMode.HBlank;
+                }else
+                {
+                    _scanline++;
+                    _dots -= 456;
+                }
+            }
+            foreach(var consumer in FrameConsumers)
+            {
+                consumer(_frame, out uint timeUsed);
+            }
+        }
+
+        private void RenderBackground()
+        {
+
+        }
+
+        private void RenderWindow()
+        {
+
+        }
+
+        private void RenderObj()
+        {
+
         }
     }
 }
