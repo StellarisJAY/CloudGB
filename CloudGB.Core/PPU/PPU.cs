a4068c5404dd60fee4fea67c0ba61bf085694276
xxjay <760409558@qq.com>
2024-05-24 11:14:16 +0800

    feat: init ppu

---
+++ CloudGB.Core/PPU/PPU.cs
@@ -0,0 +1,18 @@
+﻿namespace CloudGB.Core.PPU
+{
+    public class PPU
+    {
+        public static readonly int FrameWidth = 160;
+        public static readonly int FrameHeight = 144;
+
+        public delegate void FrameConsumer(byte[] frame, out uint timeUsed);
+
+        public List<FrameConsumer> FrameConsumers { get; private set; }
+
+        private LCDControll _lcdControll;
+        private LCDStatus _lcdStatus;
+
+
+
+    }
+}
